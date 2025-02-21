882031e58438849da4e8f444cb8c2793a0ecf5f3
Jeff <jcanfield@supplyone.com>
2025-02-21 16:38:26 -0500

    changes bell

---
+++ EWR-disableexchangeprotocols.ps1
@@ -0,0 +1,16 @@
+# Import the CSV file
+$Users = Import-Csv -Path "C:\CSV\Prod\EWR\EWRdisable.csv"
+
+# Loop through each user and disable protocols
+foreach ($User in $Users) {
+    $Email = $User.EmailAddress
+    Write-Host "Disabling protocols for $Email" -ForegroundColor Yellow
+
+    Set-CASMailbox -Identity $Email -OWAEnabled $false
+    Set-CASMailbox -Identity $Email -ActiveSyncEnabled $false
+    Set-CASMailbox -Identity $Email -ImapEnabled $false
+    Set-CASMailbox -Identity $Email -PopEnabled $false
+    Set-CASMailbox -Identity $Email -MAPIEnabled $false
+}
+
+Write-Host "Protocols disabled for all users in CSV!" -ForegroundColor Green
